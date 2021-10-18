f09348b47633d3f3e7805ecdd3ef3ac4abf4501d
PillowR <cecile.lepillouer@yahoo.com>
2021-10-18 14:41:43 +0200

    v2

--- Pojeet/Models/AnnonceService.cs
+++ Pojeet/Models/AnnonceService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Pojeet.Models
+﻿namespace Pojeet.Models
 {
     public class AnnonceService : AnnonceBesoin
     {
--- Pojeet/Models/GestionnairePlateforme.cs
+++ Pojeet/Models/GestionnairePlateforme.cs
@@ -3,8 +3,13 @@ namespace Pojeet.Models
 {
     public class GestionnairePlateforme
     {
-        public GestionnairePlateforme()
-        {
-        }
+        public short GestionairePlateforme_Id { get; set; }
+        public String Nom { get; set; }
+        public String Prenom { get; set; }
+        public String CategorieAnnonce { get; set; } // enumération 
+
+        //TODO méthode pour valider ou refuser un compte consumer
+
+        //TODO methode pour créer des services dans le catalogue de service location vente service
     }
 }

9bda6b77a924893939ca8356578925a7f706ab22
PillowR <cecile.lepillouer@yahoo.com>
2021-10-20 11:54:43 +0200

    v1

--- Pojeet/Models/CompteConsumer.cs
+++ Pojeet/Models/CompteConsumer.cs
@@ -12,10 +12,11 @@ namespace Pojeet.Models
 
         //[Required(ErrorMessage = "Le pseudo doit être rempli.")]
         //[Display(Name = "Pseudo")]
+        [Display(Name = "Pseudo")]
         public string Pseudo { get; set; }
 
 
-        [Required(ErrorMessage = "Le mot de passe doit être rempli.")]
+        //[Required(ErrorMessage = "Le mot de passe doit être rempli.")]
         [Display(Name = "Mot de passe")]
         public string MotDePasse { get; set; }
 

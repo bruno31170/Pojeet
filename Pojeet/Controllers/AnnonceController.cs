1436daae1349a16ccf2fe752b1a04433b3e638e8
PillowR <cecile.lepillouer@yahoo.com>
2021-10-26 11:30:23 +0200

    v2

--- Pojeet/Controllers/AnnonceController.cs
+++ Pojeet/Controllers/AnnonceController.cs
@@ -11,16 +11,14 @@ namespace Pojeet.Controllers
 {
     public class AnnonceController : Controller
     {
-
         private IDalAnnonce idal;
 
-        
         public AnnonceController()
         {
             this.idal = new DalAnnonce();
         }
-        
-        
+
+
         public IActionResult PosterAnnonce()
         {
             return View();
@@ -29,17 +27,17 @@ namespace Pojeet.Controllers
         [HttpPost]
         public ActionResult PosterAnnonce(UtilisateurViewModel uvm)
         {
-            
-                if (!ModelState.IsValid)
-                   return View("Error");
-               idal.PosterAnnonce(uvm.Anonce.TypeDeAnnonce, uvm.Anonce.TitreAnnonce, uvm.Anonce.Description, uvm.Anonce.DateParution,
-                   uvm.Anonce.Localisation, uvm.Anonce.DateButoir, uvm.Anonce.Prix, uvm.Anonce.CategorieDeAnnonce, uvm.Anonce.Photo);
-            
+
+            if (!ModelState.IsValid)
+                return View("Error");
+            idal.PosterAnnonce(uvm.Anonce.TypeDeAnnonce, uvm.Anonce.TitreAnnonce, uvm.Anonce.Description, uvm.Anonce.DateParution,
+                uvm.Anonce.Localisation, uvm.Anonce.DateButoir, uvm.Anonce.Prix, uvm.Anonce.CategorieDeAnnonce, uvm.Anonce.Photo);
+
             return View("Reussi"); //Retourner view mes annonces
         }
 
-       
-        
+
+
         public ActionResult SupprimerAnnonce(int id)
         {
             idal.SupprimerAnnonce(id);

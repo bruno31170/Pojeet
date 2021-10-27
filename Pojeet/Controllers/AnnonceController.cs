3d759c5c4e44f8166cf29163054c87b644431583
bruno31170 <bru.boulet@gmail.com>
2021-10-27 09:52:24 +0200

    Mercredi 10/27

--- Pojeet/Controllers/AnnonceController.cs
+++ Pojeet/Controllers/AnnonceController.cs
@@ -34,19 +34,19 @@ namespace Pojeet.Controllers
         [HttpPost]
         public ActionResult PosterAnnonce(UtilisateurViewModel uvm)
         {
-            List<Annonce> annonce = dal1.ObtientAnnonce();
-            Annonce derniereAnnonce = annonce.Last();
-            int id = derniereAnnonce.Id + 1;
+            //List<Annonce> annonce = dal1.ObtientAnnonce();
+           // Annonce derniereAnnonce = annonce.Last();
+            //int id = derniereAnnonce.Id + 1;
 
             /*if (!ModelState.IsValid)   Je ne comprends pas pourquoi le modelState est InValide alors que toutes mes conditions dans annonces sont remplis (Bruno)
                return View("Error");*/
             dal.PosterAnnonce(uvm.Anonce.TypeDeAnnonce, uvm.Anonce.TitreAnnonce, uvm.Anonce.Description, uvm.Anonce.DateParution,
                    uvm.Anonce.Localisation, uvm.Anonce.DateButoir, uvm.Anonce.Prix, uvm.Anonce.CategorieDeAnnonce, uvm.Anonce.Photo, uvm.Anonce.EtatAnnonce);
 
-            List<Annonce> annonces = dal1.ObtientAnnonce();
+            
 
-            return View("Views/Profil/Index.cshtml");
-            //return View("Views/Catalogue/AnnonceCatalogue.cshtml");
+            //return View("Views/Profil/Index.cshtml");
+            return View("AnnonceCatalogue", "Catalogue", new { id = uvm.Anonce.Id });
 
         }
 

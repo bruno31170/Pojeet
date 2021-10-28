bf25f4ef0415375021928d6bacbea00c1039d520
ines-gui <60513000+ines-gui@users.noreply.github.com>
2021-10-28 09:35:18 +0200

    Ines-28-matin

---
+++ Pojeet/Models/IDalLaisserAvis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pojeet.Models
+{
+    public interface IDalLaisserAvis
+    {
+        Profil ObtientProfil(int id);
+        void EnregistrerAvis(Avis avis);
+        void ActualiserNoteGlobale(Avis avis);
+    }
+}
---
+++ Pojeet/wwwroot/js/js-e/profil.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    console.log("ready to start PROFIL!");
+
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("activeToogle");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+
+
+    if (!String.IsNullOrEmpty(Request.QueryString["error"])) {
+        lblError.Text = "Whatever text you like.";
+    }
+
+
+});

01b92df538606598fe352fa67616e1684b1bd949
bruno31170 <bru.boulet@gmail.com>
2021-10-28 14:50:45 +0200

    Jeudi 10/28

--- Pojeet/Models/Recherche.cs
+++ Pojeet/Models/Recherche.cs
@@ -27,10 +27,7 @@ namespace Pojeet.Models
     }
     public enum CategorieRecherche
     {   
-<<<<<<< Updated upstream
         Catégorie,
-=======
->>>>>>> Stashed changes
         Réparation,
         Pièce,
         Location
